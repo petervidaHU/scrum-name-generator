5ae611d4c03d7d614f9c245d0ae1c4a06ddfda70
petervida <peter_vida1@epam.com>
2023-08-02 10:58:29 +0200

    aws dynamoDB implemented

--- app/AWSDynamoDatabase.ts
+++ app/AWSDynamoDatabase.ts
@@ -1,7 +1,11 @@
-import { GetItemCommand, ScanCommand, PutItemCommand, BatchWriteItemCommand, DeleteItemCommand, AttributeValue } from '@aws-sdk/client-dynamodb';
-import { DatabaseAbstraction, Item, createItemsReturnValue, dbClientType, deleteItemReturnValue, getAllItemsReturnValue, getItemReturnValue } from './types/databaseAbstaraction';
+import { GetItemCommand, ScanCommand, BatchWriteItemCommand, DeleteItemCommand, AttributeValue, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { DatabaseAbstraction, createItemsReturnValue, dbClientType, deleteItemReturnValue, getAllItemsReturnValue, getItemReturnValue } from './types/databaseAbstaraction';
 import { iNameItemComplete } from './types/nameTypes';
-import { stringify } from 'querystring';
+
+type normalizedTagsObject = { [key: string]: string[] };
+type DynamoDBItem = Record<string, AttributeValue>;
+type tagsTwinPeaks = { itemsToUpdate: DynamoDBItem[]; itemsToCreate: DynamoDBItem[] }
 
 export class AWSDynamoDatabase implements DatabaseAbstraction {
   namesTable: string;
@@ -53,10 +57,8 @@ export class AWSDynamoDatabase implements DatabaseAbstraction {
     return items ? items : [];
   }
 
-  private handleTags(items: iNameItemComplete[]) {
-    type normalizedTagsObject = { [key: string]: string[] };
-    
-    const nomalizedTags: any = items.reduce((acc, item): normalizedTagsObject => {
+  private normalizedTags(items: iNameItemComplete[]) {
+    return items.reduce((acc, item): normalizedTagsObject => {
       item.tags.forEach(tag => {
         if (acc[tag] && !acc[tag].includes(item.name)) {
           acc[tag].push(item.name);
@@ -69,19 +71,108 @@ export class AWSDynamoDatabase implements DatabaseAbstraction {
       })
       return acc;
     }, {} as normalizedTagsObject)
-
-    console.log('nomalizedTags: ', nomalizedTags);
-
-    // return
   }
 
-  async createItem(items: iNameItemComplete[]): createItemsReturnValue {
-    if (!this.namesTable) return 'no table';
+  private async separateNewOrUpdate(list: normalizedTagsObject): Promise<tagsTwinPeaks> {
+    return Object.entries(list).reduce(
+      async (accPromise: Promise<tagsTwinPeaks>, [tagName, tagItems]) => {
+        const acc = await accPromise;
 
-    this.handleTags(items);
+        const getItemCommand = new GetItemCommand({
+          TableName: this.tagsTable,
+          Key: {
+            tagName: { S: tagName },
+            status: { S: 'active' } || { S: 'inactive' },
+          },
+        });
 
+        const existingTag = await this.client.send(getItemCommand);
+
+        if (existingTag.Item) {
+          return {
+            ...acc,
+            itemsToUpdate: [...acc.itemsToUpdate, existingTag.Item],
+          };
+        } else {
+          const convertedItem: DynamoDBItem = {
+            tagName: { S: tagName },
+            status: { S: 'active' },
+            items: { SS: tagItems },
+          };
+          return {
+            ...acc,
+            itemsToCreate: [...acc.itemsToCreate, convertedItem],
+          };
+        }
+      },
+      Promise.resolve({ itemsToUpdate: [], itemsToCreate: [] })
+    );
+  }
+
+  private async createNewTags(tagsToCreate: DynamoDBItem[]) {
+    let newTagsResponse: string;
+    console.log('tagsToCreate: ', tagsToCreate)
+
+    const params = {
+      RequestItems: {
+        [this.tagsTable]: tagsToCreate.map(item => ({
+          PutRequest: { Item: item },
+        })),
+      },
+      ConditionExpression: 'attribute_not_exists(#n)',
+      ExpressionAttributeNames: {
+        '#n': 'tagName',
+      },
+    };
+    const command = new BatchWriteItemCommand(params);
+    await this.client.send(command);
+
+    return 'new tags created ok';
+  }
+
+  private async updateTags(itemsToUpdate: DynamoDBItem[]) {
+    for (const itemFocused of itemsToUpdate) {
+      console.log('it: ', itemFocused)
+      const paramsUpdate = {
+        TableName: this.tagsTable,
+        Key: {
+          tagName: itemFocused.tagName,
+          status: itemFocused.status,
+        },
+        UpdateExpression: 'ADD #items :newItem',
+        ExpressionAttributeNames: {
+          '#items': 'items',
+        },
+        ExpressionAttributeValues: {
+          ':newItem': itemFocused.items,
+        },
+        ConditionExpression: 'NOT contains(#items, :newItem)',
+      };
+
+      const commandUpdate = new UpdateItemCommand(paramsUpdate);
+      await this.client.send(commandUpdate);
+    }
+
+    return 'tags updated successfully';
+  }
+
+  private async handleTags(items: iNameItemComplete[]) {
+    const { itemsToUpdate, itemsToCreate } = await this.separateNewOrUpdate(
+      this.normalizedTags(items)
+    );
+    if (itemsToCreate.length) {
+      await this.createNewTags(itemsToCreate);
+    }
+    if (itemsToUpdate.length) {
+      await this.updateTags(itemsToUpdate);
+    }
+
+    return 'tags ok';
+  }
+
+  private async createNewNames(items: iNameItemComplete[]) {
     const putRequestsName = items.map((item) => {
-      const convertedItem: Record<string, AttributeValue> = {
+      const convertedItem: DynamoDBItem = {
         name: { S: item.name },
         description: { S: item.description },
         status: { S: 'active' },
@@ -91,23 +182,26 @@ export class AWSDynamoDatabase implements DatabaseAbstraction {
       })
     });
 
-
     const params = {
       RequestItems: {
         [this.namesTable]: putRequestsName,
       },
-      ReturnConsumedCapacity: 'TOTAL',
-      ReturnItemCollectionMetrics: 'SIZE',
       ConditionExpression: 'attribute_not_exists(#n)',
       ExpressionAttributeNames: {
         '#n': 'name'
       }
     };
+
     const command = new BatchWriteItemCommand(params);
+    await this.client.send(command);
+  }
+
+  async createItem(items: iNameItemComplete[]): Promise<string> {
+    if (!this.namesTable || !this.tagsTable) return 'no table';
 
     try {
-      const response = await this.client.send(command);
-      console.log('Items created successfully:', response);
+      // await this.createNewNames(items);
+      await this.handleTags(items);
       return 'Items created successfully';
     } catch (err) {
       console.log('Error creating items:', err);
